0922f804f97a4a1367ff581dc6638bdc3e5c92d7
polariis <polariis@163.com>
2022-10-26 15:45:03 +0800

    增加pinia

--- src/main.js
+++ src/main.js
@@ -1,42 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import setupStore from './store';
 import directives from './directives'; //导入自定义指令
 // import '@/assets/less/index.less';
-// import 'animate.css';
 import 'animate.css';
 
-const app = createApp(App, () => {
-  // 实例创建完成后被立即调用rem换算方法,解决内容闪现问题
-  // if (
-  //   navigator.userAgent.match(
-  //     /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-  //   )
-  // ) {
-  //   setRem();
-  //   window.addEventListener('resize', setRem); //浏览器窗口大小改变时调用rem换算方法
-  // } else {
-  //   setRemPC();
-  //   window.addEventListener('resize', setRemPC); //浏览器窗口大小改变时调用rem换算方法
-  // }
-});
-app.use(router).mount('#app');
-directives(app);
+const app = createApp(App);
 
-// app.create(() => {
-//   // 实例创建完成后被立即调用rem换算方法,解决内容闪现问题
-//   if (
-//     navigator.userAgent.match(
-//       /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-//     )
-//   ) {
-//     setRem();
-//     window.addEventListener('resize', setRem); //浏览器窗口大小改变时调用rem换算方法
-//   } else {
-//     setRemPC();
-//     window.addEventListener('resize', setRemPC); //浏览器窗口大小改变时调用rem换算方法
-//   }
-// });
+app.use(router).mount('#app');
+directives(app); // 注册全局自定义指令
+setupStore(app);
 
 // rem适配：
 if (
