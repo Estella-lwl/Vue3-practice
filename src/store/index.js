e4a33f62c483331d0d3d060a7ae22c221258c667
polariis <polariis@163.com>
2022-11-05 16:37:34 +0800

    some code to review

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createStore } from 'vuex';
 
 const store = createStore({
@@ -9,3 +10,15 @@ const store = createStore({
 });
 
 export default store;
+=======
+import { App } from 'vue';
+import { createPinia } from 'pinia';
+
+const store = createPinia();
+
+export function setupStore(app) {
+  app.use(store);
+}
+
+export { store };
+>>>>>>> 0922f804f97a4a1367ff581dc6638bdc3e5c92d7
